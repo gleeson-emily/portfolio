b39fa57c14aa2c01ceb963e2f7ee251f9c8dbc60
Emily Gleeson <engleeson@gmail.com>
2021-09-02 20:36:39 -0400

    adding to abuot me page

--- src/components/About.js
+++ src/components/About.js
@@ -13,15 +13,22 @@ export default function About(){
         <img className="headshot" src={otherImgs.headshot}/>
         <div className="card aboutCard">
         <div className="block">
-        <p>Hi there! I'm Emily, and I'm an aspiring web developer currently based in Toronto, Ontario. After being laid off from an unfulfilling position in Public Relations, I decided to make a career change into web development.</p>
-        </div>
-        <div className="block">
-        <p>I'm proficient in many technologies, but I prefer working on back-end code. Working with databases is one of my favourite parts of coding!</p>
+        <p>I'm an full-stack web developer currently based in Toronto, Ontario. I previously completed a Bachelor of Arts in Film Studies and a Master of Arts in Film Studies, both at Carleton University, and recently completed a certificate in Full-Stack Web Development through the University of Toronto.</p>
         </div>
         <div className="block">
         <p>Check out the Portfolio tab for some examples of my work, and my details are on the Contact tab if you want to get in touch.</p>
                 </div>
              </div>
+        <div className="block">
+            <div className="card aboutCard">
+            <h5>Technical Skills</h5>
+            <br />
+            <p className="skills"><i>Programming Languages:</i> HTML, CSS, JavaScript</p>
+            <p className="skills"><i>Libraries and Frameworks:</i> jQuery, React, Express, Bootstrap, Bulma</p>
+            <p className="skills"><i>Database and Back-End Management:</i> MySQL, MongoDB, GraphQL</p>
+            <p className="skills"><i>Other Technologies:</i> Node.js, Git, Heroku, JWT Auth, Web APIs</p>
+                </div>     
+            </div>
         </div>
     )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,13 @@ export default function Footer() {
     return (
 <footer className="footer">
     <p>Thanks for visiting!! 💖
-<a href="https://www.linkedin.com/in/emily-gleeson24" target="_blank">
+<a href="https://www.linkedin.com/in/emily-gleeson24" target="_blank" rel="noreferrer">
         LinkedIn
       </a>
-    <a href="https://github.com/gleeson-emily" target="_blank">
+    <a href="https://github.com/gleeson-emily" target="_blank" rel="noreferrer">
         GitHub
       </a>
-<a href="https://stackoverflow.com/users/15508690/emilyg">Stack Overflow</a>
+<a href="https://stackoverflow.com/users/15508690/emilyg" target="_blank" rel="noreferrer">Stack Overflow</a>
 </p>
 </footer>
     )
